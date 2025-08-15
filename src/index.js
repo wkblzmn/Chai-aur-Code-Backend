23f43563e7a7307ce5213607c8c0db0ffa0d9753
Wakibul Zaman <wkblzmn@gmail.com>
2025-08-15 19:07:15 +0600

    06

---
+++ src/index.js
@@ -0,0 +1,38 @@
+import dotenv from 'dotenv'
+import connectDB from "./db/index.js";
+
+dotenv.config({ path: '/.env' })
+connectDB();
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+import express from "express";
+const app = express();
+
+(async () => {
+    try{
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_Name}`)
+        app.on("error", () => {
+            console.log("ERROR: ", error);
+            throw error;
+        })
+
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${process.env.PORT}`);
+        })
+
+    } catch (error) {
+        console.error("Error connecting to MongoDB. Error: ", error);
+    }
+}) ()
+*/
